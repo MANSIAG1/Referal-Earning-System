f69ac466a9e743c03dbe859343361e8e2b48ab14
MANSIAG1 <agarwalmansi764@gmail.com>
2026-02-13 14:41:39 +0530

    added cors - domains

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware 
 app.use(cors({
-    origin: process.env.FRONTEND_URL,
+    origin: ["https://luxestream.asprin.dev", "https://referal-earning-system.vercel.app", "http://localhost:5173", process.env.FRONTEND_URL],
     credentials: true
 }));
 app.use(express.json());
